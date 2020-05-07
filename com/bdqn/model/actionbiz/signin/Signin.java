3689271e8daad9e0e612628f7f79785420016e84
ButchQiuQiu <133465202@qq.com>
2020-05-07 21:57:08 +0800

    移植

--- com/bdqn/model/actionbiz/signin/Signin.java
+++
@@ -1,8 +0,0 @@
-package com.bdqn.model.actionbiz.signin;
-
-import com.bdqn.controller.Action;
-//传入一个用户签到的命令,使用session中的user作为凭借;
-public abstract class Signin implements Action {
-	
-	protected boolean returnIsTrue; //签到成功后返回true
-}
