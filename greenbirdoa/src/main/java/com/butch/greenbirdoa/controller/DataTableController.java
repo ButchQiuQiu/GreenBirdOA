ebc97379ebf054994bd255ad136737d2939214eb
ButchQiuQiu <133465202@qq.com>
2020-05-11 17:27:18 +0800

    修复前端

--- greenbirdoa/src/main/java/com/butch/greenbirdoa/controller/DataTableController.java
+++ greenbirdoa/src/main/java/com/butch/greenbirdoa/controller/DataTableController.java
@@ -1,6 +1,13 @@
 package com.butch.greenbirdoa.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.butch.greenbirdoa.mapper.DepartMapper;
+import com.butch.greenbirdoa.mapper.JurisdictionMapper;
+import com.butch.greenbirdoa.mapper.SexMapper;
 import com.butch.greenbirdoa.mapper.UserMapper;
+import com.butch.greenbirdoa.pojo.User;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -16,9 +23,24 @@ public class DataTableController {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private SexMapper sexMapper;
+    @Autowired
+    private DepartMapper departMapper;
+    @Autowired
+    private JurisdictionMapper jurisdictionMapper;
+    
+    /**
+     * 获取当前角色能获取到的用户和所有的fk
+     */
     @GetMapping("data/table/user")
-    public String getAllUser() throws JsonProcessingException {
+    public String getAllUserAndFkByUserRole() throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.writeValueAsString(userMapper.getAllUser());
+        List<Object> reJson = new ArrayList<>();
+        reJson.add(sexMapper.getAllSex());
+        reJson.add(departMapper.getAllDepart());
+        reJson.add(jurisdictionMapper.getAllJurisdiction());
+        reJson.addAll(userMapper.getAllUser());
+        return objectMapper.writeValueAsString(reJson);
     }
 }
